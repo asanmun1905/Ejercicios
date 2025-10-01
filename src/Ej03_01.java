69b0f168a714e4b24e8518f13d37b551dff8ebe1
Adrian Sanchez M. <adri1952005.4@gmail.com>
2025-10-01 14:42:37 +0200

    added ex 3 unit 3

---
+++ src/Ej03_01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ej03_01 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Este programa multiplica dos números enteros.");
+        System.out.println("Por favor, introduzca el primer número: ");
+        int num1 = s.nextInt();
+        System.out.println("Introduzca el segundo número: ");
+        int num2 = s.nextInt();
+        System.out.printf("%d * %d = %d", num1, num2, num1 * num2);
+
+    }
+}
